496e06f3900eb2aaa6b1afe2e13e5f06a26908f1
Javier Ramírez Jr <xbenek@java.net>
2013-03-01 16:15:18 -0600

    Adición de la clase de dominio Voto. Se elimina placeholder.

---
+++ grails-app/domain/org/javamexico/portal/contenido/Voto.groovy
@@ -0,0 +1,13 @@
+package org.javamexico.portal.contenido
+
+class Voto {
+
+    Boolean up
+
+    Date dateCreated
+    Date lastUpdated
+    Date dateDeleted
+
+    static constraints = {
+    }
+}
