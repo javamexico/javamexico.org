36981638eca2ca5a765a6f78ee38fb547b20aa3d
Javier Ramírez Jr <xbenek@java.net>
2013-03-04 18:59:24 -0600

    Voto pertenece a un usuario.

--- grails-app/domain/org/javamexico/portal/contenido/Voto.groovy
+++ grails-app/domain/org/javamexico/portal/contenido/Voto.groovy
@@ -1,8 +1,12 @@
 package org.javamexico.portal.contenido
 
+import org.javamexico.portal.usuarios.Usuario
+
 class Voto {
 
     Boolean up
+
+    static belongsTo = [usuario:Usuario]
 
     Date dateCreated
     Date lastUpdated
