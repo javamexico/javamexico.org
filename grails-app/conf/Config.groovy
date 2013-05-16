22d4a6a003e45117079d7742b04f41e2dbf3312f
Javier Ramírez Jr <xbenek@java.net>
2013-05-15 19:25:25 -0500

    Mappings para tipos de Joda Time

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -64,7 +64,7 @@ environments {
         grails.logging.jul.usebridge = true
         grails.dbconsole.enabled = true
         grails.dbconsole.urlRoot = '/admin/dbconsole'
-        grails.plugins.springsecurity.active = false
+        //grails.plugins.springsecurity.active = false
     }
     production {
         grails.logging.jul.usebridge = false
@@ -102,3 +102,20 @@ log4j = {
 grails.plugins.springsecurity.userLookup.userDomainClassName = 'org.javamexico.portal.usuarios.Usuario'
 grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'org.javamexico.portal.usuarios.UsuarioRol'
 grails.plugins.springsecurity.authority.className = 'org.javamexico.portal.usuarios.Rol'
+
+// Added by the Joda-Time plugin:
+grails.gorm.default.mapping = {
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentDateMidnight, class: org.joda.time.DateMidnight
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentDateTime, class: org.joda.time.DateTime
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentDateTimeZoneAsString, class: org.joda.time.DateTimeZone
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentDurationAsString, class: org.joda.time.Duration
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentInstantAsMillisLong, class: org.joda.time.Instant
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentInterval, class: org.joda.time.Interval
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentLocalDate, class: org.joda.time.LocalDate
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime, class: org.joda.time.LocalDateTime
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentLocalTime, class: org.joda.time.LocalTime
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentPeriodAsString, class: org.joda.time.Period
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentTimeOfDay, class: org.joda.time.TimeOfDay
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentYearMonthDay, class: org.joda.time.YearMonthDay
+	"user-type" type: org.jadira.usertype.dateandtime.joda.PersistentYears, class: org.joda.time.Years
+}
