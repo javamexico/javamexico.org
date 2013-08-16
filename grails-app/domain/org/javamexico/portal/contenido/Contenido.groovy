b4cb7a1dd47ec2a4f40d17a60c25b81f0ff13806
Javier Ramírez Jr <xbenek@java.net>
2013-08-16 04:45:19 -0500

    Constraints para todos los tipos de Contenido.

--- grails-app/domain/org/javamexico/portal/contenido/Contenido.groovy
+++ grails-app/domain/org/javamexico/portal/contenido/Contenido.groovy
@@ -2,24 +2,35 @@ package org.javamexico.portal.contenido
 
 import org.javamexico.portal.tags.Tag
 import org.javamexico.portal.usuarios.Usuario
-import org.joda.time.LocalDate
+import org.joda.time.DateTime
 
 abstract class Contenido {
 
     String titulo
     String texto
     String hostname
-    Integer status
+    EstatusContenidoEnum status
 
-    LocalDate dateCreated
-    LocalDate lastUpdated
-    LocalDate dateDeleted
+    //TODO Usar DateTime
+    DateTime dateCreated
+    DateTime lastUpdated
+    DateTime dateDeleted
 
     static belongsTo = [usuario:Usuario]
     static hasMany = [tags:Tag, comentarios:Comentario, votos:Voto]
 
     static constraints = {
-        dateDeleted nullable: true
+        titulo ()
+        texto widget: 'textarea', maxSize: 15000
+        hostname display: false
+        status display: false
+        dateCreated display: false
+        lastUpdated display: false
+        dateDeleted nullable: true, display: false
+        usuario nullable: true, display: false
+        tags nullable: true
+        comentarios nullable: true, display: false
+        votos nullable: true, display: false
     }
 
 }
