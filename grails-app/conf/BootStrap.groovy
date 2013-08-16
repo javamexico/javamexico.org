b4c2143b2775409c233a74241ce0dd4f25a32daa
Javier Ramírez Jr <xbenek@java.net>
2013-08-16 04:40:07 -0500

    Usuario de prueba en BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,26 @@
+import org.javamexico.portal.usuarios.Usuario
+import org.joda.time.LocalDate
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def springSecurityService
+
+        if (!Usuario.count()){
+            new Usuario(
+                    username: 'benek',
+                    password: 'benekin',
+                    enabled: true,
+                    nombre: 'Javier Ramirez',
+                    correo: 'xbenek@java.net',
+                    fechaNacimiento: new LocalDate(1984, 3, 20),
+                    ubicacion: 'Queretaro',
+                    sitioWeb: 'www.javamexico.org/blogs/benek',
+                    bio: 'Algo aqui...'
+            ).save(failOnError: true)
+        }
+
     }
     def destroy = {
     }
